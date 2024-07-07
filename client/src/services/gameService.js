596d823f521ce604b6dfb50b525b05971089ea5a
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-07-07 17:06:14 +0300

    Modify request function

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,24 +1,27 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 
 const BASE_URL = 'http://localhost:3030/jsonstore/games'
 
 export const getAll = async () => {
-    const result = await request('GET', BASE_URL);
+    const result = await request.get(BASE_URL);
 
     // console.log(result);
     return Object.values(result);
 }
 
 export const create = async (gameData) => {
-    const response = await fetch(BASE_URL, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(gameData)
-    });
 
-    const result = response.json();
+    const result = await request.post(BASE_URL, gameData);
 
     return result;
+    // const response = await fetch(BASE_URL, {
+    //     method: 'POST',
+    //     headers: {
+    //         'content-type': 'application/json'
+    //     },
+    //     body: JSON.stringify(gameData)
+    // });
+
+    // const result = response.json();
+
 }
