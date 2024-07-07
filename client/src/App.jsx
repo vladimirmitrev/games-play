b47433c8b9ca76ef63fc0c5f1395a9c8df039612
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-07-07 15:44:06 +0300

    Add routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
+import Header from "./components/header/Header"
+import Home from "./components/home/Home"
+import Login from "./components/login/Login"
+import {Routes, Route} from "react-router-dom"
+import Register from "./components/register/Register"
+import GamesList from "./components/games-list/GameList"
+import GameCreate from "./components/game-create/GameCreate"
 
 
 function App() {
 
   return (
     <>
-      <h1>Games Play</h1>
+      <div id="box">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/games" element={<GamesList />}/>
+          <Route path="/games/create" element={<GameCreate />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+        </Routes>
+      </div>
     </>
   )
 }
