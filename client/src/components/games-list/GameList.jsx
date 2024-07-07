cc696ebc80c69445069931cb78621e3cfd524f1d
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-07-07 16:52:26 +0300

    Add dynamic game list

--- client/src/components/games-list/GameList.jsx
+++ client/src/components/games-list/GameList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import * as gamesService from '../../services/gameService'
+import GameListItem from "./game-list-item/GameListItem";
 
 const GamesList = () => {
     const [games, setGames] = useState([]);
@@ -14,36 +15,14 @@ const GamesList = () => {
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            {/* <!-- Display div: with information about every game (if any) --> */}
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
+            {games.map(game => (
+                <GameListItem key={game._id} {...game}/>
+            ))}
 
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
+            {games.length === 0 && (
+                <h3 className="no-articles">No articles yet</h3>
+            )}
 
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
-
-            {/* <!-- Display paragraph: If there is no games  --> */}
-            <h3 className="no-articles">No articles yet</h3>
         </section>
     );
 }
---
+++ client/src/components/games-list/game-list-item/GameListItem.jsx
@@ -0,0 +1,21 @@
+import {Link} from 'react-router-dom';
+
+const GameListItem = ({
+    _id,
+    title,
+    category,
+    imageUrl,
+}) => {
+    return (
+        <div className="allGames">
+                <div className="allGames-info">
+                    <img src={imageUrl} />
+                    <h6>{category}</h6>
+                    <h2>{title}</h2>
+                    <Link to={`/games/${_id}`} className="details-button">Details</Link>
+                </div>
+            </div>
+    );
+}
+
+export default GameListItem;
