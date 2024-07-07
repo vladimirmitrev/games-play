d5d38ce3876e0f01971e0ac14308eb270c168b05
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-07-07 17:34:19 +0300

    Add game details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import {Routes, Route} from "react-router-dom"
 import Register from "./components/register/Register"
 import GamesList from "./components/games-list/GameList"
 import GameCreate from "./components/game-create/GameCreate"
+import GameDetails from "./components/game-details/GameDetails"
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Route path="/games/create" element={<GameCreate />}/>
           <Route path="/login" element={<Login />}/>
           <Route path="/register" element={<Register />}/>
+          <Route path="/games/:gameId" element={<GameDetails />} />
         </Routes>
       </div>
     </>
