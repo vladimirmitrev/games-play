a312e7516d32f1a39efa98aa42f348c26b1bbbc9
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-07-10 15:45:43 +0300

    Modify login to controlled form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,23 +1,45 @@
-const Login = () => {
-    return (
-        <section id="login-page" className="auth">
-            <form id="login">
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Login</h1>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+import useForm from '../../hooks/useForm';
 
-                    <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
-                    <input type="submit" className="btn submit" value="Login" />
-                    <p className="field">
-                        <span>If you don`t have profile click <a href="#">here</a></span>
-                    </p>
-                </div>
-            </form>
-        </section>
-    );
-}
+const Login = () => {
+  const { values, onChange, onSubmit } = useForm({
+    email: '',
+    password: '',
+  });
+
+  return (
+    <section id="login-page" className="auth">
+      <form id="login" onSubmit={onSubmit}>
+        <div className="container">
+          <div className="brand-logo"></div>
+          <h1>Login</h1>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Sokka@gmail.com"
+            onChange={onChange}
+            value={values.email}
+          />
+
+          <label htmlFor="login-pass">Password:</label>
+          <input
+            type="password"
+            id="login-password"
+            name="password"
+            onChange={onChange}
+            value={values.password}
+          />
+          <input type="submit" className="btn submit" value="Login" />
+          <p className="field">
+            <span>
+              If you don`t have profile click <a href="#">here</a>
+            </span>
+          </p>
+        </div>
+      </form>
+    </section>
+  );
+};
 
 export default Login;
